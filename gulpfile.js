828747807a7718e38ddb8eb2fdcb37cfc4ae2a3b
Virus007 <virusoo7@hotmail.com>
2019-01-05 12:27:56 +0200

    add nested and var and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested');
+	
 
 gulp.task('default', function(){
 	console.log("this is gulp task");
@@ -11,7 +16,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe()
+		.pipe(postcss([cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
