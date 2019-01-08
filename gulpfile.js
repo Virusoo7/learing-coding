a7b8fc198ec28f8f4498a598cb8ea134ee49f14e
Virus007 <virusoo7@hotmail.com>
2019-01-08 11:27:30 +0200

    update btn and apply browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer'),
 	cssvars = require('postcss-simple-vars'),
-	nested = require('postcss-nested');
+	nested = require('postcss-nested'),
+	cssIimport = require('postcss-import'),
+	browserSync = require('browser-sync').create();
 	
 
 gulp.task('default', function(){
@@ -16,17 +18,27 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssIimport, cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
+	browserSync.init({
+		notify:false,
+		server: {
+			baseDir: "app"
+		}
+	});
 	watch('./app/index.html', function(){
-		gulp.start('html');
+		browserSync.reload();
 	});
 	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('styles');
+		gulp.start('cssInject');
 	});
 
 });
 
+gulp.task('cssInject',['styles'] , function(){
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});
